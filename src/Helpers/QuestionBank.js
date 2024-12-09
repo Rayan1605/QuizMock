1171649bcd579416ce9ccd5e20ba8e2b87e4722f
Rayan1605 <rayanzyad99@gmail.com>
2024-12-09 21:26:51 +0000

    Go

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,10 @@
+export const Questions = [
+    {
+        prompt: "What is 1+3?",
+        optionA: "24",
+        optionB: "32",
+        optionC: "4",
+        optionD: "2",
+        answer: "C",
+    },
+];
